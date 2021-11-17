1ef3137acf6b6b291e43b478d1e19761e28a0028
xypage <xypage@gmail.com>
2021-11-16 19:35:16 -0800

    Refactored so we don't loop through same numbers over and over

--- superAverage.py
+++ superAverage.py
@@ -2,100 +2,146 @@
 from math import pow, sqrt
 import random
 
-#test
+class MeanFunction:
+	def __init__(self, data, preprocessing, processing, postprocessing):
+		self.data = data
+		self.preprocessing = preprocessing
+		self.processing = processing
+		self.postprocessing = postprocessing
 
-def harmonicMean(arr):
-	denominator = 0
-	for num in arr:
-		denominator += 1 / num
-	return len(arr) / denominator
 
-def geometricMean(arr):
-	gm = 1
-	gmPower = 1 / len(arr)
-	for num in arr:
-		gm *= pow(num, gmPower)
-	return gm
+def harmonicMean():
+	data = 0                          # Stores sum of reciprocals
+	def preprocessing(self, arr):
+		self.length = len(arr)          # Stores the length
+	def processing(self, num):
+		self.data += 1/num              # Sums the reciprocals
+	def postprocessing(self):
+		return self.length / self.data	# Returns the final calculation
+	
+	return MeanFunction(data, preprocessing, processing, postprocessing)
 
-def rootMeanSquare(arr):
-	rms = 0
-	l = len(arr)
-	for num in arr:
-		rms += num * num / l
-	return sqrt(rms)
-
+# Added this because we get the mean for the standard deviation elsewhere so it's nice as its own function
 def arithmeticMean(arr):
 	s = 0
 	for num in arr:
 		s += num
 	return s / len(arr)
+def arithmeticMeanObj():
+	data = 0                          # Stores the sum
+	def preprocessing(self, arr):
+		self.length = len(arr)          # Stores the length
+	def processing(self, num):
+		self.data += num                # Adds to the sum
+	def postprocessing(self):
+		return self.data / self.length  # returns the final calculation
 
+	return MeanFunction(data, preprocessing, processing, postprocessing)
+
+def geometricMean():
+	data = 1
+	def preprocessing(self, arr):
+		self.length = len(arr)
+	def processing(self, num):
+		self.data *= num
+	def postprocessing(self):
+		return pow(self.data, 1/self.length)
+
+	return MeanFunction(data, preprocessing, processing, postprocessing)
+
+def rootMeanSquare():
+	data = 0
+	def preprocessing(self, arr):
+		self.length = len(arr)
+	def processing(self, num):
+		self.data += num * num / self.length
+	def postprocessing(self):
+		return sqrt(self.data)
+
+	return MeanFunction(data, preprocessing, processing, postprocessing)
+
+# Separated this out since mode uses it when there's no unique values
 def median(arr):
-	return arr[int(len(arr) / 2)]
+	l = len(arr)
+	if(l % 2 == 0):
+		return (arr[int(l / 2)] + arr[int(l / 2) - 1]) / 2
+	else:
+		return arr[int(l / 2)]
 
-def mode(arr):
-	occurences = {}
-	someDuplicates = False
-	for num in arr:
-		if(num in occurences):
-			occurences[num] += 1
-			someDuplicates = True
-		else:
-			occurences[num] = 1
-
-	if(not someDuplicates):
-		return median(arr)
+def medianObj():
+	def preprocessing(self, arr):
+		self.data = median(arr)
+	def processing(self, num):
+		return
+	def postprocessing(self):
+		return self.data
 	
-	maxValue = occurences[arr[0]]
-	maxKey = arr[0]
-	for (key, value) in occurences.items():
-		if(value > maxValue):
-			maxValue = value
-			maxKey = key
-	return maxKey
+	return MeanFunction(0, preprocessing, processing, postprocessing)
+
+def mode():
+	def preprocessing(self, arr):
+		occurences = {}
+		someDuplicates = False
+		for num in arr:
+			if(num in occurences):
+				occurences[num] += 1
+				someDuplicates = True
+			else:
+				occurences[num] = 1
+
+		if(not someDuplicates):
+			self.data = median(arr)
+		
+		maxValue = occurences[arr[0]]
+		maxKey = arr[0]
+		for (key, value) in occurences.items():
+			if(value > maxValue):
+				maxValue = value
+				maxKey = key
+		self.data = maxKey
+	def processing(self, num):
+		return
+	def postprocessing(self):
+		return self.data
+
+	return MeanFunction(0, preprocessing, processing, postprocessing)
+
+
 
 # Leaving out mode because after the first iteration, all the values are floats and almost certainly unique so it ends up just being median again
-averageFunctions = (
+meanFunctions = (
 	(harmonicMean, "harmonic mean"),
 	(geometricMean, "geometric mean"),
 	(rootMeanSquare, "root mean square"),
-	(arithmeticMean, "arithmetic mean"),
-	(median, "median"),
+	(arithmeticMeanObj, "arithmetic mean"),
+	(medianObj, "median"),
 	# (mode, "mode")
 )
 
+
+
 def superAvg(arr):
 	if(len(arr) == 0):
-		return [0] * len(averageFunctions)
+		return [0] * len(meanFunctions)
+
+	methods = []
+	for i in range(len(meanFunctions)):
+		methods.append(meanFunctions[i][0]())
+		methods[i].preprocessing(methods[i], arr)
+
+	for num in arr:
+		for method in methods:
+			method.processing(method, num)
+	
 	averages = []
-	for (method, name) in averageFunctions:
-		averages.append(method(arr))
+	for method in methods:
+		averages.append(method.postprocessing(method))
+	
 	return averages
-
-def oldSuperAvg(arr):
-	if(len(arr) == 0):
-		return [0, 0, 0, 0, 0]
-	median = arr[int(len(arr)/2)]
-	product = 1
-	sum = 0
-	geometricMean = 1
-	geometricMeanPower = 1/len(arr)
-	harmonicDenominator = 0
-	rootMeanSquare = 0
-	for i in arr:
-		product *= i
-		sum += i
-		geometricMean *= pow(i, geometricMeanPower)
-		harmonicDenominator += 1/i
-		rootMeanSquare += i * i / len(arr)
-
-	arithmeticMean = sum / len(arr)
-	harmonicMean = len(arr) / harmonicDenominator
-	rootMeanSquare = sqrt(rootMeanSquare)
-	return [harmonicMean, geometricMean, rootMeanSquare, arithmeticMean, median] 
 
 def standardDeviation(arr):
 	dist = 0
+
 	mean = arithmeticMean(arr)
 	for num in arr:
 		dist += pow(mean - num, 2)
@@ -119,28 +165,36 @@ def superSuperAvg(arr, allowedDiff):
 			nextArr.sort()
 	return (arr, iterations)
 
+
+
+
 if __name__ == "__main__":
+	print("Average functions are: [", end="")
+	for type in meanFunctions[:-1]:
+		print(f"{type[1]}, ", end="")
+	print(f"{meanFunctions[-1][1]}]\n")
+
+
 	arr = []
 	for i in range(100):
 		arr.append(random.randrange(1,1000))
 
-	arr.sort()
-	print(f"initial array:\n\t{arr}\n")
 
-	print("Average functions are: [", end="")
-	for type in averageFunctions[:-1]:
-		print(f"{type[1]}, ", end="")
-	print(f"{averageFunctions[-1][1]}]\n")
+	arr.sort()
+	print(f"Initial array:\n\t{arr}\n")
+
 
 	firstAverage = superAvg(arr)
 	print(f"First super average:\n\t{firstAverage}")
-	(finalAverages, iterations) = superSuperAvg(arr, 10 ** -5)
-	print(f"After {iterations} iterations we reached a final set of averages of:\n\t{finalAverages}\n")
 
+
+	(finalAverages, iterations) = superSuperAvg(arr, 10 ** -5)
 	finalCheckValue = arithmeticMean(finalAverages)
+	print(f"After {iterations} iterations we reached a final average of ~{round(finalCheckValue, 4)}, or specifically the set:\n\t{finalAverages}\n")
+
+	
 	closest = [abs(finalCheckValue - firstAverage[0]), 0]
 	for i in range(len(firstAverage)):
 		if(abs(finalCheckValue - firstAverage[i]) < closest[0]):
 			closest = [finalCheckValue - firstAverage[i], i]
-	print(f"Closest was {firstAverage[closest[1]]}, the {averageFunctions[closest[1]][1]}.")
-	
+	print(f"Closest was {firstAverage[closest[1]]}, the {meanFunctions[closest[1]][1]}.")
